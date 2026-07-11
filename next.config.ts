bdf1fa740d724875d7316a986f404a3af683f9fb
aranisarora <82080947+aranisarora@users.noreply.github.com>
2026-07-12 00:00:20 +0530

    feat: redirect old domain to sharwinacademy.com

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,22 @@ const withPWA = require("next-pwa")({
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "sharwin-tabletennisacademy.com" }],
+        destination: "https://sharwinacademy.com/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.sharwin-tabletennisacademy.com" }],
+        destination: "https://sharwinacademy.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withPWA(nextConfig);
